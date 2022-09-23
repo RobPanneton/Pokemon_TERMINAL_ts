a1153ae9113f87449378c8564514be6f0a39a799
RobPanneton <robert.panneton@gmail.com>
2022-09-23 18:25:30 -0400

    move intro to class

--- app/teambuilder/tbMenuPrompts/tbGetSpeciesInput.ts
+++ app/teambuilder/tbMenuPrompts/tbGetSpeciesInput.ts
@@ -2,11 +2,13 @@ import { userInputPrompt } from "../../utils/prompts";
 import { isOnlyNumbers } from "../../utils/validators";
 
 export const tbGetNewSpeciesInput = (pokemonListString, validInputs) => {
-  console.log(`${pokemonListString}   B) Go Back   E) Exit \n`);
+  console.log(`${pokemonListString}   R) Restart   E) Exit \n`);
   let userInput = userInputPrompt("Choose a Pokemon: ");
 
   if (userInput.toUpperCase() === "E") process.exit();
-  if (userInput.toUpperCase() === "B") return "B";
+  if (userInput.toUpperCase() === "R") return "R";
+  // idea for returning to mainmenu, try to see if process.abort or something similar would stop this function from continuing
+  // could not return start() since the function call can't accept a promise as a returned value
 
   if (!isOnlyNumbers(userInput)) userInput = userInput.toUpperCase();
 
