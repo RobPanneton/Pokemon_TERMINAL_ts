ac012817399afafff0c61b4482fde7cabb23a490
RobPanneton <robert.panneton@gmail.com>
2022-09-30 19:22:27 -0400

    fix bug

--- app/battle/battleUtils/getNpcMove.ts
+++ app/battle/battleUtils/getNpcMove.ts
@@ -1,4 +1,4 @@
 export const getNpcMove = (attacks) => {
   if (attacks.length === 1) return attacks[0];
-  return Math.floor(Math.random() * (attacks.length - 1 + 1) + 1);
+  return attacks[Math.floor(Math.random() * (attacks.length - 1 + 1) + 1) - 1];
 };
